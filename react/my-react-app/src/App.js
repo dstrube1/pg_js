513b0ef286a66b96b2342c50f0b535b3dd79d817
David Strube <dstrube3@gatech.edu>
2025-08-04 14:31:25 -0400

    React progress

--- react/my-react-app/src/App.js
+++ react/my-react-app/src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+//If most of the html code in function App() is commented out, then these imports are no longer needed:
+//import logo from './logo.svg';
+//import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+	/* 
+	This is what was between the header tag, with my new code right after the p tag and before the a tag:
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        Make a change here, save the file, <br/>
+        and the change can* show up immediately with no need to reload the browser! <br />
+        (*Usually)
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,8 +20,13 @@ function App() {
         >
           Learn React
         </a>
+	*/
+  return (
+    <div className="App">
+      <header className="App-header">
+    	<h1>Test</h1>
       </header>
-    </div>
+    </div>     
   );
 }
 
