8d954146f5924a0ae6703a6a9001130ac4357da4
David Strube <dstrube3@gatech.edu>
2025-08-15 08:16:43 -0400

    WIP

--- react/my-react-app/src/index.js
+++ react/my-react-app/src/index.js
@@ -15,3 +15,46 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals(console.log);
+
+//Classes and Inheritance
+class Test{
+	constructor(text){
+		this.content = text;
+	}
+	
+	printTest(){
+		return 'Test class content: ' + this.content;
+	}
+}
+
+class Child extends Test{
+	constructor(text, stuff){
+		super(text);
+		this.thing = stuff;
+	}
+	
+	doSomething(){
+		return 'Parent: ' + this.printTest() + '; kid stuff: ' + this.thing;
+	}
+}
+
+const child = new Child("1", "2");
+console.log(child.doSomething());
+
+//Arrow functions:
+f1 = function(){
+	return "f1";
+}
+
+f2 = () => {
+	return "f2";
+}
+
+f3 = () => "f3"; //this only works if the function has only 1 statement
+
+f4 = (val) => "f4 with value passed in: " + val;
+
+//this only works if there is only 1 parameter:
+f5 = val => "f5 with value passed in without parentheses: " + val; 
+
+console.log(f1);
